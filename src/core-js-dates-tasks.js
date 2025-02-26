f32a09031b2dccfffb763e103037a9c3e85eea85
solowihmaria <solowihmaria@gmail.com>
2025-02-26 11:48:00 +0300

    feat: add solution of 10th task

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -219,8 +219,19 @@ function getCountWeekendsInMonth(month, year) {
  * Date(2024, 0, 31) => 5
  * Date(2024, 1, 23) => 8
  */
-function getWeekNumberByDate(/* date */) {
-  throw new Error('Not implemented');
+function getWeekNumberByDate(date) {
+  const dateCopy = new Date(
+    Date.UTC(date.getFullYear(), date.getMonth(), date.getDate())
+  );
+
+  const dayNum = dateCopy.getUTCDay() === 0 ? 7 : dateCopy.getUTCDay();
+
+  dateCopy.setUTCDate(dateCopy.getUTCDate() + 4 - dayNum);
+
+  const yearStart = new Date(Date.UTC(dateCopy.getUTCFullYear(), 0, 1));
+  const weekNumb = Math.ceil(((dateCopy - yearStart) / 86400000 + 1) / 7);
+
+  return weekNumb;
 }
 
 /**
